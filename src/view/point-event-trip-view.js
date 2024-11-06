afb2bf6e9d6d74fc5149d5c936e7c49eb2c11fa8
bearBenjamin <git@github.com:bearBenjamin/cinemaddict.git>
2024-11-06 12:41:26 +0300

    закончил второй шаг

--- src/view/point-event-trip-view.js
+++ src/view/point-event-trip-view.js
@@ -3,7 +3,7 @@ import {createElement} from '../render.js';
 import { duration, getOffer, showDate, showMachineDate, showMachineTime, showTime } from '../util.js';
 
 const createPointEventTripTemplate = (point) => {
-  const { basePrice, dateFrom, dateTo, isFavorite, type, offers } = point;
+  const { basePrice, destination, dateFrom, dateTo, isFavorite, type, offers } = point;
 
   const dateMachine = showMachineDate(dateFrom);
   const date =showDate(dateFrom);
@@ -61,7 +61,7 @@ const createPointEventTripTemplate = (point) => {
   <div class="event__type">
   <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
   </div>
-  <h3 class="event__title">Taxi Amsterdam</h3>
+  <h3 class="event__title">${type} ${destination}</h3>
   <div class="event__schedule">
   <p class="event__time">
   <time class="event__start-time" datetime="${dateFromMachine}">${timeFrom}</time>
